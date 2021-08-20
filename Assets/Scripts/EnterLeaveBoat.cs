428efe44998d6ffc69620b5d8c7dd7bc6fcf8cca
Sean Siders <sean.siders@icloud.com>
2021-08-19 23:03:08 -0700

    Boat docking and animation logic
    Basic boat docking system is now implemented. Animation logic is present, but I believe we need new animations with the same model, or a custom skeletal mesh to get them all working together. Currently, the walking animation is working, and the idle is a "dummy" animation that freezes the mesh. This at least looks intentional :+1:

---
+++ Assets/Scripts/EnterLeaveBoat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterLeaveBoat : MonoBehaviour
+{
+    public GameObject boat;
+
+    //True when player is in the boat
+    private bool inBoat = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //When the player is in the boat, have the boat follow player movement
+        if (inBoat) mirrorPlayerMovement(gameObject, boat);
+    }
+
+    public void enterBoat()
+    {
+        Debug.Log("ENTERED BOAT");        
+        inBoat = true;
+    }
+
+    public void leaveBoat()
+    {
+        Debug.Log("LEFT BOAT");        
+        inBoat = false;
+    }
+
+    public bool isInBoat()
+    {
+        return inBoat;
+    }
+
+    //Boat explicity follows player movement and rotation
+    //This behaivor only occurs when |inBoat| is true
+    //TODO : child index crap dependency
+    private static void mirrorPlayerMovement(GameObject Player, GameObject boat)
+    {
+        boat.transform.position = Player.transform.position;
+        boat.transform.rotation = Player.transform.GetChild(0).rotation;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Animator PlayerAnims;
 
     public float speed = 12f;
     private float gravity = 14f;
     private float verticalVelocity;
+    private bool isMoving = false;
 
     // Update is called once per frame
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        isMoving = !(x == 0f && z == 0f);
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        //Trigger running animation if any movement input is recieved
+        PlayerAnims.SetBool("isMoving", isMoving);
 
-        controller.Move(move * speed * Time.deltaTime);
+        //If there is input, update the player position and rotation
+        if (isMoving)
+        {
+            Vector3 move = transform.right * x + transform.forward * z;
+            Quaternion rotation = Quaternion.LookRotation(move, Vector3.up);
+
+            controller.Move(move * speed * Time.deltaTime);
+
+            //TODO : The player must be at index 0, this is a crap dependency and should be fixed
+            transform.GetChild(0).rotation = rotation;
+        }
 
         if(controller.isGrounded)
         {
